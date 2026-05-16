2ef735f8630f88c88259ed8b975bd73d5f2c43bc
tristanpassaglia2-jpg <tristanpassaglia2@gmail.com>
2026-05-15 23:52:24 -0300

    Simplify routing by removing LivePage component
    Removed routing for LivePage and simplified the structure.

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,10 +1,9 @@
 // apps/web/src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
-import LivePage from './live';
 import './index.css';
 
 const queryClient = new QueryClient({
@@ -17,10 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/live/:token" element={<LivePage />} />
-          <Route path="*" element={<App />} />
-        </Routes>
+        <App />
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
